3f3901ef6d6270cf593e847710ab7b7c533d1e7e
MD RAHAT <rshf01791@gmail.com>
2025-10-24 01:40:33 +0600

    compare_two_numbers.c

--- compare_two_numbers.c
+++ compare_two_numbers.c
@@ -1,5 +1,5 @@
 /*
-https://codeforces.com/group/MWSDmqGsZm/contest/219158/my
+https://www.hackerrank.com/contests/mid-term-a-introduction-to-c-programming-a-batch-08/challenges/count-me-2-1/submissions/code/1399385388
 */
 /*
 Given two numbers A and B. Print "Yes" if A is greater than or equal to B. Otherwise print "No".
@@ -42,4 +42,5 @@ https://www.codechef.com/users/rahat01791
 /*
 Codeforces profile link :
 https://codeforces.com/profile/RAHAT01791
+
 */
